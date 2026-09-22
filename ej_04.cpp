aa2459b10f8ab73f384fb48411d4ba496831ecd6
RD <darkreingreso@gmail.com>
2026-09-22 12:52:21 -0500

    Version final

--- ej_04.cpp
+++ ej_04.cpp
@@ -5,6 +5,7 @@ int main()
 {
 int A[10];
     int n,i,j,aux;
+    int iz=0;
 
     cout << "Ingresa la cantidad de numeros: ";
     cin >> n;
@@ -18,6 +19,35 @@ int A[10];
     {
         cout<<A[i]<<" ";
 	}
+	
+	int de=n-1;
+	
+   for(i=0;i<n-1;i++){
+        for(j=iz;j<de;j++){
+            if(A[j]>A[j + 1]){
+                aux=A[j];
+                A[j]=A[j+1];
+                A[j+1]=aux;
+            }
+        }
+        de--;
+        for(j=de;j>iz;j--){
+            if(A[j]<A[j-1]){
+                aux=A[j];
+                A[j]=A[j-1];
+                A[j-1]=aux;
+            }
+        }
+        iz++;
+    }
+    
+    cout <<"\n\nArreglo ordenado: "<<endl;
+
+
+    for(i=0;i<n;i++)
+    {
+        cout<<A[i]<<" ";
+    }
     
    
     return 0;
