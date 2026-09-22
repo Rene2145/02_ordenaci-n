99f18ec6313b62607b1962ba0ea5be5d7434cde8
RD <darkreingreso@gmail.com>
2026-09-22 05:54:15 -0500

    Version final

--- ej_01.cpp
+++ ej_01.cpp
@@ -3,6 +3,41 @@ using namespace std;
 
 int main()
 {
+int A[10];
+    int n,i,j,aux;
+
+    cout << "Ingresa la cantidad de numeros: ";
+    cin >> n;
+    cout << "Ingresa los numeros:" << endl;
+    for(i=0;i<n;i++)
+    {
+	cin >> A[i];
+    }
+    cout <<"\nArreglo desordenado:"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<A[i]<<" ";
+	}
+    
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(A[j]>A[j + 1])
+            {
+                aux=A[j];
+                A[j]=A[j+1];
+                A[j+1]=aux;
+            }
+        }
+    }
+    cout <<"\n\nArreglo ordenado:"<<endl;
+
+
+    for(i=0;i<n;i++)
+    {
+        cout<<A[i]<<" ";
+    }
 
     return 0;
 }
